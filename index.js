d047e769c77a8ef67b2acaa5c0e7e8048eaaba41
Duc Thanh Vu <73021414+thanhvd1403@users.noreply.github.com>
2024-12-09 22:58:49 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { Client, Intents, Collection } = require("discord.js");
+const { Player } = require("discord-player");
+
+const fs = require("fs");
+const path = require("path");
